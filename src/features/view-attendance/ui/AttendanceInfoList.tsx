7b31aa53d737e1b3a53d0a670bda79e5c8f88b08
KimYouGyoung <tptkds12@gmail.com>
2024-10-04 00:40:05 +0900

    feat: 근무 기록 아이템

--- src/features/view-attendance/ui/AttendanceInfoList.tsx
+++ src/features/view-attendance/ui/AttendanceInfoList.tsx
@@ -1,11 +1,36 @@
+/* eslint-disable arrow-parens */
+import AttendanceInfoItem, { AttendanceItem } from './AttendanceInfoItem'
+
 export default function AttendanceInfoList() {
-  const data: unknown[] = []
-  if (data.length === 0) {
+  const attendanceItems: AttendanceItem[] = [
+    {
+      id: '1',
+      date: '9월 12일 (월)',
+      time: '09:00 - 18:00',
+    },
+    {
+      id: '2',
+      date: '9월 12일 (월)',
+      time: '09:00 - 18:00',
+    },
+    {
+      id: '3',
+      date: '9월 12일 (월)',
+      time: '09:00 - 18:00',
+    },
+  ]
+  if (attendanceItems.length === 0) {
     return (
       <div className="w-full pt-[120px] flex items-center justify-center">
         <p className="text-[##6F6F6F] body-02-medium">근무 정보가 아직 없어요</p>
       </div>
     )
   }
-  return <div>ff</div>
+  return (
+    <div className="px-[16px]">
+      {attendanceItems.map(item => (
+        <AttendanceInfoItem item={item} key={item.id} />
+      ))}
+    </div>
+  )
 }
--- src/features/view-attendance/ui/DateSelector.tsx
+++ src/features/view-attendance/ui/DateSelector.tsx
@@ -1,19 +1,27 @@
 'use client'
 
+/* eslint-disable max-len */
+
 import { useState } from 'react'
-import formatDate from '../utils/formateDate'
 import PrevButton from './PrevButton'
 import NextButton from './NextButton'
-import changeDate from '../utils/changeDate'
 
 export default function DateSelector({ type }: { type: 'week' | 'month' }) {
-  const [selectedDate, setSelectedDate] = useState<Date>(new Date())
+  const [year, setYear] = useState<number>(new Date().getFullYear())
+  const [month, setMonth] = useState<number>()
+  const [weekOfMonth, setWeekOfMonth] = useState<number>()
 
   return (
     <div className="flex space-x-2 justify-between body-04-bold-compact items-center">
-      <PrevButton onClick={() => changeDate(type, selectedDate, setSelectedDate)} />
-      <p>{formatDate(selectedDate, type)}</p>
-      <NextButton onClick={() => changeDate(type, selectedDate, setSelectedDate)} />
+      <PrevButton />
+      {type === 'week' ? (
+        <p>{`${month || ''}월 ${weekOfMonth || ''}주차`}</p>
+      ) : (
+        <p>
+          {year || ''}년 {month || ''}월
+        </p>
+      )}
+      <NextButton />
     </div>
   )
 }
