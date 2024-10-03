16895bebbf2512f93ef12f700150732a87a81b0c
yerim08 <sbnlover1008@naver.com>
2024-10-03 21:23:29 +0900

    FEAT: KAKAO 직원 로그인 flow

--- src/app/join/kakao/page.tsx
+++ src/app/join/kakao/page.tsx
@@ -1,18 +1,44 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import FlexBox from '@/component/shared/flexbox'
 import BadgeCard from '@/widgets/join/ui/BadgeCard'
 import { ButtonMobile, TopBar } from '@eolluga/eolluga-ui'
+import { useSetAtom } from 'jotai'
+import { isOwnerAtom } from '@/lib/globalState'
 
 export default function KakaoPage() {
+  const router = useRouter()
+  const setIsOwner = useSetAtom(isOwnerAtom)
+
   return (
-    <FlexBox direction="col" className="bg-white w-full h-full items-center w-full h-full relative ">
-      <TopBar leftIcon="chevron_left_outlined" />
+    <FlexBox direction="col" className="bg-white w-full h-full items-center relative ">
+      <TopBar leftIcon="chevron_left_outlined" onClickLeftIcon={() => router.back()} />
       <div className="w-full head-02-bold px-spacing-04 text-left mt-spacing-06 mb-[66px]">직원으로 근무 중인가요?</div>
       <BadgeCard />
       <FlexBox direction="col" className="w-full px-spacing-04 py-spacing-03 gap-spacing-02">
-        <ButtonMobile size="L" state="enabled" style="primary" text1="네 직원이에요" type="text" />
-        <ButtonMobile size="M" state="disabled" style="ghost" text1="아니요 사장님이에요" type="text" />
+        <ButtonMobile
+          size="L"
+          state="enabled"
+          style="primary"
+          text1="네 직원이에요"
+          type="text"
+          onClick={() => {
+            setIsOwner(false)
+            router.push('/join/kakao/employee/1')
+          }}
+        />
+        <ButtonMobile
+          size="M"
+          state="enabled"
+          style="ghost"
+          text1="아니요 사장님이에요"
+          type="text"
+          onClick={() => {
+            setIsOwner(true)
+            router.push('/join/kakao/owner/1')
+          }}
+        />
       </FlexBox>
     </FlexBox>
   )
