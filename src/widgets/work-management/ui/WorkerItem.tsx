f7cb41202e30a23f3f3605f1501020f0f86d74c0
KimYouGyoung <tptkds12@gmail.com>
2024-10-07 01:21:30 +0900

    fix: 활성화된 Avatar 테두리 표시 및 밝게 표시되도록 수정

--- src/widgets/work-management/ui/WorkerItem.tsx
+++ src/widgets/work-management/ui/WorkerItem.tsx
@@ -16,11 +16,13 @@ export default function WorkerItem({ worker }: { worker: Worker }) {
   const [selectedWorkerIDAtom, setSelectedWorkerIDAtom] = useAtom(selectedWorkerAtom)
   return (
     <button
-      className="h-[88px] w-[48px] flex flex-col space-y-1 items-center justify-end text-center"
+      className="h-[90px] w-[48px] flex flex-col space-y-1 items-center justify-end text-center"
       type="button"
       onClick={() => setSelectedWorkerIDAtom(worker.id)}
     >
-      <Avatar size="S" image={worker?.image || undefined} border={selectedWorkerIDAtom === worker.id} />
+      <span className={`${selectedWorkerIDAtom === worker.id ? 'border border-white rounded-full' : 'opacity-70'}`}>
+        <Avatar size="S" image={worker?.image || undefined} />
+      </span>
       <div>
         <p
           className={`body-02-medium-compact  ${selectedWorkerIDAtom === worker.id ? 'text-white' : 'text-[#8D8D8D]'}`}
