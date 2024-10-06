99201783f3f777a1a34cb2a49cc4923e3482e81d
KimYouGyoung <tptkds12@gmail.com>
2024-10-07 01:27:01 +0900

    fix: WorkerDragScrollBox가 모든 width를 차지하도록 수정

--- src/widgets/work-management/ui/WorkerList.tsx
+++ src/widgets/work-management/ui/WorkerList.tsx
@@ -5,8 +5,11 @@ import WorkerItem, { Worker } from './WorkerItem'
 export default function WorkerList({ workers }: { workers: Worker[] }) {
   return (
     <DragScrollWrapper>
-      {workers.map(worker => (
-        <WorkerItem worker={worker} key={worker.id} />
+      {workers.map((worker, i) => (
+        <>
+          {i === 0 && <span className="w-[16px] h-1" />}
+          <WorkerItem worker={worker} key={worker.id} />
+        </>
       ))}
     </DragScrollWrapper>
   )
