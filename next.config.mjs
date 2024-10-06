495492362d0b0ac054305ad30692d41e77936fac
yerim08 <sbnlover1008@naver.com>
2024-10-06 21:59:22 +0900

    FIX: svg 파일 분리

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { transpilePackages: ['jotai-devtools'] }
+const nextConfig = {
+  transpilePackages: ['jotai-devtools'],
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+}
 
 export default nextConfig
