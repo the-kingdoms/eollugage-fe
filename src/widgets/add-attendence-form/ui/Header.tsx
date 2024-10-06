8231487074240ab99eb50d3599a3b0d7588c428e
KimYouGyoung <tptkds12@gmail.com>
2024-10-07 00:30:51 +0900

    fix: 근무 추가 페이지 오버플로우 문제 수정

--- src/widgets/add-attendence-form/ui/Header.tsx
+++ src/widgets/add-attendence-form/ui/Header.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="h-[32px] text-center relative flex items-center justify-center py-[6px] font-medium mt-[12px] mb-[32px]">
+    <header className="h-[32px] text-center relative flex items-center justify-center  font-medium pt-[18px] mb-[32px]">
       <Link
-        className="absolute left-[14px] top-1/2 -translate-y-1/2"
+        className="absolute left-[14px] top-1/2 -translate-y-1/2 pt-[18px] "
         type="button"
         aria-label="뒤로가기"
         href="/manage"
