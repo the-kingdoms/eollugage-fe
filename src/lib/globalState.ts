bb79faca2f92ad15589b91b0ea4eaab17e17a55b
yerim08 <sbnlover1008@naver.com>
2024-10-07 19:23:11 +0900

    FEAT: 로그인 방식 atom

--- src/lib/globalState.ts
+++ src/lib/globalState.ts
@@ -3,3 +3,4 @@ import { atom } from 'jotai'
 // 사장이면 true, 직원이면 false
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const isOwnerAtom = atom<boolean>(true)
+export const loginMethodAtom = atom<'apple' | 'kakao' | 'phone'>('apple')
