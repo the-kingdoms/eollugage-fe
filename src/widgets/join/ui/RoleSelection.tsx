e184e4a06152e1d6453277382c27f57ea61392a5
yerim08 <sbnlover1008@naver.com>
2024-10-07 19:25:06 +0900

    FIX: 직원/사장 선택 페이지 컴포넌트로 분리

---
+++ src/widgets/join/ui/RoleSelection.tsx
@@ -0,0 +1,17 @@
+import FlexBox from '@/component/shared/flexbox'
+import { ButtonMobile } from '@eolluga/eolluga-ui'
+import React from 'react'
+import BadgeCard from './BadgeCard'
+
+export default function RoleSelection() {
+  return (
+    <>
+      <div className="w-full head-02-bold px-spacing-04 text-left mt-spacing-06 mb-[66px]">직원으로 근무 중인가요?</div>
+      <BadgeCard />
+      <FlexBox direction="col" className="w-full px-spacing-04 py-spacing-03 gap-spacing-02">
+        <ButtonMobile size="L" state="enabled" style="primary" text1="네 직원이에요" type="text" />
+        <ButtonMobile size="M" state="disabled" style="ghost" text1="아니요 사장님이에요" type="text" />
+      </FlexBox>
+    </>
+  )
+}
