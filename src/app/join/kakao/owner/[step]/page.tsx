baa9a6b4963baf73f75f995c380454a2c648b096
yerim08 <sbnlover1008@naver.com>
2024-10-07 19:31:57 +0900

    FIX: lint 오류 수정

--- src/app/join/kakao/owner/[step]/page.tsx
+++ src/app/join/kakao/owner/[step]/page.tsx
@@ -148,7 +148,7 @@ export default function KakaoOwnerPage() {
       {/* BottomSheet */}
       {openBottomSheet && (
         <Scrim className="fixed inset-0 z-40 flex items-center justify-center" onClick={handleCloseBottomSheet}>
-          <BottomSheet onClose={handleCloseBottomSheet} open={openBottomSheet}>
+          <BottomSheet open={openBottomSheet}>
             <FlexBox direction="col" className="w-full text-left items-start gap-spacing-01">
               <div className="body-04-bold">가게 코드는 홈에서 확인할 수 있어요</div>
               <div className="body-01-regular-compact text-[#6F6F6F]">
@@ -157,14 +157,14 @@ export default function KakaoOwnerPage() {
               </div>
             </FlexBox>
             <div className="w-full mt-[48px] mb-[80px]">
-              <TextField
+              {/* <TextField
                 value="aaaa"
                 label="가게 코드"
                 size="L"
                 style="underlined"
                 state="readOnly"
-                onChange={() => console.log('')}
-              />
+                onChange={() => }
+              /> */}
             </div>
             <div className="w-full">
               <ButtonMobile
--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,3 +1,17 @@
+'use client'
+
+import FlexBox from '@/component/shared/flexbox'
+import RoleSelection from '@/widgets/join/ui/RoleSelection'
+import SignupName from '@/widgets/join/ui/SignupName'
+import { TopBar } from '@eolluga/eolluga-ui'
+import { useState } from 'react'
+
 export default function JoinPage() {
-  return <div>회원가입</div>
+  return (
+    <FlexBox direction="col" className="bg-white w-full h-full items-center w-full h-full relative ">
+      <TopBar leftIcon="chevron_left_outlined" />
+      <RoleSelection />
+      <SignupName />
+    </FlexBox>
+  )
 }
