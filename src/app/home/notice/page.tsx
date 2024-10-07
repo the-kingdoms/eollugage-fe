bc4f788ae82ff8d863653ac8b9f9f0ea8658152e
PangKiYeon <pdrkeamy@gmail.com>
2024-10-07 13:10:03 +0900

    FIX : notice 버튼 로직 수정

--- src/app/home/notice/page.tsx
+++ src/app/home/notice/page.tsx
@@ -3,14 +3,27 @@
 import FlexBox from '@/component/shared/flexbox'
 import { ButtonMobile, TextArea, TopBar } from '@eolluga/eolluga-ui'
 import { useRouter } from 'next/navigation'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useState, useEffect } from 'react'
 
 export default function NoticePage() {
   const router = useRouter()
   const [noticeText, setNoticeText] = useState('')
+  const [buttonText, setButtonText] = useState('추가하기')
+
+  useEffect(() => {
+    if (noticeText.trim()) { // api 연결 후 디테일하게 수정 예정
+      setButtonText('수정하기')
+    } else {
+      setButtonText('추가하기')
+    }
+  }, [])
 
   const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setNoticeText(event.target.value)
+  }
+
+  const handleFocus = () => {
+    setButtonText('저장하기')
   }
 
   return (
@@ -23,11 +36,11 @@ export default function NoticePage() {
         }}
       />
       <FlexBox direction="col" className="w-full mt-8 px-4 h-full justify-between">
+        <div className='w-full' onFocus={handleFocus}>
         <TextArea size="L" placeholder="공지가 아직 없어요" value={noticeText} onChange={handleTextChange} />
-        <ButtonMobile size="L" style="primary" type="text" state="enabled" text1="저장하기" />
+        </div>
+        <ButtonMobile size="L" style="primary" type="text" state="enabled" text1={buttonText} />
       </FlexBox>
     </FlexBox>
   )
 }
-
-// 데이터 연결 후 버튼 수정
