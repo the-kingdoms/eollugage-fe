10a5e82542a3431f65c94ae3f7a63c98e87b3fe9
yerim08 <sbnlover1008@naver.com>
2024-10-08 15:35:23 +0900

    FIX: join 폴더 구조 정리

---
+++ src/app/join/employee/page.tsx
@@ -0,0 +1,10 @@
+import { SignupName } from '@/widgets'
+import React from 'react'
+
+export default function EmployeeJoinPage() {
+  return (
+    <>
+      <SignupName />
+    </>
+  )
+}
--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import FlexBox from '@/component/shared/flexbox'
-import RoleSelection from '@/widgets/join/ui/RoleSelection'
-import SignupName from '@/widgets/join/ui/SignupName'
+import { RoleSelection } from '@/widgets'
 import { TopBar } from '@eolluga/eolluga-ui'
 
 export default function JoinPage() {
@@ -10,7 +9,6 @@ export default function JoinPage() {
     <FlexBox direction="col" className="bg-white w-full h-full items-center w-full h-full relative ">
       <TopBar leftIcon="chevron_left_outlined" />
       <RoleSelection />
-      <SignupName />
     </FlexBox>
   )
 }
