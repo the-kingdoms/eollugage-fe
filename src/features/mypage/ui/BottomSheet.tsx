7e8063df07233081e4b82858c1679b010d8f7924
Wonchang0314 <andrew0314@naver.com>
2024-10-11 02:54:00 +0900

    FIX: 린트 에러 수정

--- src/features/mypage/ui/BottomSheet.tsx
+++ src/features/mypage/ui/BottomSheet.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line react/jsx-curly-brace-presence */
 import { PositionGroupType } from '@/shared/types/myPageTypes'
 import { v4 as uuidv4 } from 'uuid'
 import { Button } from '@/shared/ui/shadcn/button'
