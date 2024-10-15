dabbf358dd8020e7f1687c560ffe3b40bb1b0cf9
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-10-15 22:28:35 +0900

    fix: 이미지 업로드 화면 버튼 라우팅 수정

--- src/features/upload-image/ui/UploadImageScreen.tsx
+++ src/features/upload-image/ui/UploadImageScreen.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation'
 import { ButtonMobile, Icon, TopBar } from '@eolluga/eolluga-ui'
 import { sendRNFunction } from '../utils/rnSender'
 import { ImageUploadResultT } from '../types/imageUploadType'
+import Link from 'next/link'
 
 interface ImageUploadScreenProps {
   page: 'home' | 'join'
@@ -79,9 +80,12 @@ export default function ImageUploadScreen({ page }: ImageUploadScreenProps) {
           onClick={onClickSelectButton}
         />
         <FlexBox className="w-full justify-center">
-          <button type="button" className="py-3 label-02-bold text-text-disabled">
+          <Link
+            href={page === 'join' ? '/home' : '/home'}
+            className="py-3 label-02-bold text-text-disabled"
+          >
             나중에 추가하기
-          </button>
+          </Link>
         </FlexBox>
       </FlexBox>
     </FlexBox>
