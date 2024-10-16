1b1a60f7ca70c7f8e6ef15e1041107e87960a366
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-10-16 19:14:59 +0900

    fix: RN으로 함수 파라미터도 보낼 수 있게 수정

--- src/features/upload-image/utils/rnSender.ts
+++ src/features/upload-image/utils/rnSender.ts
@@ -1,7 +1,7 @@
-export function sendRNFunction(funcName: string) {
+export function sendRNFunction(funcName: string, param?: string) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { ReactNativeWebView } = window as any
   if (ReactNativeWebView) {
-    ReactNativeWebView.postMessage(JSON.stringify({ type: funcName }))
+    ReactNativeWebView.postMessage(JSON.stringify({ type: funcName, data: param }))
   }
 }
