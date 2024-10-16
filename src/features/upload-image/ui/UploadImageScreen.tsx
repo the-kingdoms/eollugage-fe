2739dc7208352f399a23bb7c6da87a826fa86f86
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-10-16 20:40:30 +0900

    refactor: rn 통신 handleImageUpload 함수 분리

--- src/features/upload-image/ui/UploadImageScreen.tsx
+++ src/features/upload-image/ui/UploadImageScreen.tsx
@@ -10,6 +10,7 @@ import { ImageUploadResultT } from '../types/imageUploadType'
 import Image from 'next/image'
 import { ToastMessage } from '@/shared'
 import { getImageFromS3 } from '../api/getImage'
+import { handleImageUpload } from '../utils/handleImageUpload'
 
 interface ImageUploadScreenProps {
   page: 'home' | 'join'
@@ -18,7 +19,6 @@ interface ImageUploadScreenProps {
 export default function ImageUploadScreen({ page }: ImageUploadScreenProps) {
   const router = useRouter()
 
-  // eslint-disable-next-line
   const [isSuccess, setIsSuccess] = useState<boolean>(false)
   const [showToast, setShowToast] = useState<boolean>(false)
   const [errorMessage, setErrorMessage] = useState<string>('')
@@ -37,33 +37,8 @@ export default function ImageUploadScreen({ page }: ImageUploadScreenProps) {
     const message: { type: string; data: ImageUploadResultT } = JSON.parse(String(e.data))
 
     if (message.type === 'getImageUploadResult') {
-      if (!message.data.isSuccess) {
-        const failReason = message.data.reason
-        switch (failReason) {
-          case 'upload-fail':
-            setErrorMessage('업로드에 실패했습니다.')
-            break
-          case 'not-select':
-            setErrorMessage('사진이 선택되지 않았습니다.')
-            break
-          case 'presigned-url-error':
-            setErrorMessage('다시 시도해주세요.')
-            break
-          default:
-            setErrorMessage('다시 시도해주세요.')
-        }
-        setShowToast(true)
-      } else {
-        getImageFromS3(String(message.data.fileFullName)).then(url => {
-          if (url.length !== 0) {
-            setIsSuccess(true)
-            setImageURL(url)
-          } else {
-            setErrorMessage('다시 시도해주세요.')
-            setShowToast(true)
-          }
-        })
-      }
+      const data = message.data
+      handleImageUpload({ data, setErrorMessage, setIsSuccess, setShowToast, setImageURL })
     }
   }
 
---
+++ src/features/upload-image/utils/handleImageUpload.ts
@@ -0,0 +1,49 @@
+import React, { SetStateAction } from 'react'
+import { ImageUploadResultT } from '../types/imageUploadType'
+import { getImageFromS3 } from '../api/getImage'
+
+interface HandleImageUploadParams {
+  data: ImageUploadResultT
+  setErrorMessage: React.Dispatch<SetStateAction<string>>
+  setIsSuccess: React.Dispatch<SetStateAction<boolean>>
+  setShowToast: React.Dispatch<SetStateAction<boolean>>
+  setImageURL: React.Dispatch<SetStateAction<string>>
+}
+
+function handleImageUpload({
+  data,
+  setErrorMessage,
+  setIsSuccess,
+  setShowToast,
+  setImageURL,
+}: HandleImageUploadParams) {
+  if (!data.isSuccess) {
+    const failReason = data.reason
+    switch (failReason) {
+      case 'upload-fail':
+        setErrorMessage('업로드에 실패했습니다.')
+        break
+      case 'not-select':
+        setErrorMessage('사진이 선택되지 않았습니다.')
+        break
+      case 'presigned-url-error':
+        setErrorMessage('다시 시도해주세요.')
+        break
+      default:
+        setErrorMessage('다시 시도해주세요.')
+    }
+    setShowToast(true)
+  } else {
+    getImageFromS3(String(data.fileFullName)).then(url => {
+      if (url.length !== 0) {
+        setIsSuccess(true)
+        setImageURL(url)
+      } else {
+        setErrorMessage('다시 시도해주세요.')
+        setShowToast(true)
+      }
+    })
+  }
+}
+
+export { handleImageUpload }
