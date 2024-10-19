5d688905ff0147cb7ef68bf26bc5da8bb968b76d
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-10-19 15:19:33 +0900

    fix: 사진 업로드 라우팅 경로 수정

--- src/features/home/ui/AddPhotoButton.tsx
+++ src/features/home/ui/AddPhotoButton.tsx
@@ -1,25 +1,23 @@
-'use client'
-
 import FlexBox from '@/shared/ui/Flexbox'
 import { Icon } from '@eolluga/eolluga-ui'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
-export default function AddPhotoButton() {
-  const router = useRouter()
+interface AddPhotoButtonProps {
+  storeId: string
+}
+
+export default function AddPhotoButton({ storeId }: AddPhotoButtonProps) {
   return (
-    <div
+    <Link
       className="flex flex-row items-center mx-4 mb-6 px-3 pb-[11px] pt-[9px] border border-border-title-01 rounded-lg
            justify-between"
-      onClick={() => {
-        router.push('/home/upload-image')
-      }}
-      aria-hidden="true"
+      href={`/${storeId}/home/upload-image`}
     >
       <FlexBox direction="col" className="gap-2 w-full items-start">
         <div className="body-02-bold-compact">가게 사진을 손님들이 보고싶어 해요</div>
         <div className="body-01-medium-compact text-text-helper">가게 사진 추가하러 가기</div>
       </FlexBox>
       <Icon icon="chevron_right_outlined" size={20} className="fill-icon-secondary" />
-    </div>
+    </Link>
   )
 }
