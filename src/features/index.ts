c6f40223439b224c0570dafea924d1c5f1ab96d0
PangKiYeon <pdrkeamy@gmail.com>
2024-10-20 20:43:20 +0900

    FEAT : format current date 구하는 함수 구현

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,2 +1,3 @@
 export { default as getRandomColor } from './join/utils/getRandomColor'
 export { default as UploadImageScreen } from './upload-image/ui/UploadImageScreen'
+export { default as formatCurrentDate } from './home/utils/formatCurrentDate'
