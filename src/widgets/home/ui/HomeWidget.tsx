c41bced6017d0f0f452804e297f1dff3fd29b9d2
PangKiYeon <pdrkeamy@gmail.com>
2024-10-20 20:49:56 +0900

    FEAT : 홈 페이지에 data fetch 구현

---
+++ src/widgets/home/ui/HomeWidget.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import Header from '@/widgets/home/ui/Header'
+import FlexBox from '@/shared/ui/Flexbox'
+import AddPhotoButton from '@/widgets/home/ui/AddPhotoButton'
+import HomeNotice from '@/widgets/home/ui/HomeNotice'
+import TodayWork from '@/widgets/home/ui/TodayWork'
+import OrderList from '@/widgets/home/ui/OrderList'
+import { formatCurrentDate } from '@/features'
+import { useAtom } from 'jotai'
+import { isOwnerAtom } from '@/shared'
+import { storeNameAtom } from '@/shared/atoms/globalAtom'
+import { useEffect } from 'react'
+import { useGetOrder } from '../model/useGetOrder'
+import { useGetTodayDuty } from '../model/useGetTodyDuty'
+import { useGetStoreDetail } from '../model/useGetStoreDetail'
+import { noticeAtom } from '../atoms/homeAtoms'
+
+interface HomeWidgetProps {
+  storeId: string
+}
+
+export default function HomeWidget({ storeId }: HomeWidgetProps) {
+  const [isOwner] = useAtom(isOwnerAtom)
+  const [storeName] = useAtom(storeNameAtom)
+  const [notice] = useAtom(noticeAtom)
+
+  const { data: orderList } = useGetOrder(storeId)
+  const { data: workList } = useGetTodayDuty(storeId, formatCurrentDate())
+  const { mutate: details } = useGetStoreDetail(storeId)
+
+  useEffect(() => {
+    details()
+  }, [details])
+
+  return (
+    <>
+      <Header title={storeName || ''} />
+      <div className="mt-[76px]">
+        {isOwner && <AddPhotoButton storeId={storeId} />}
+        <FlexBox direction="col" className="gap-8 mx-4">
+          <HomeNotice storeId={storeId} notice={notice || ''} />
+          <TodayWork workList={workList?.histories || []} />
+          <OrderList storeId={storeId} orderList={orderList || []} />
+        </FlexBox>
+      </div>
+    </>
+  )
+}
+
+// 버튼 여부
+// prefetch
