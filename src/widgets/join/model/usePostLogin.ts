d8d46513007caa21d68b68a89707a3b2c23d664d
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-10-22 00:23:53 +0900

    fix: RN으로 토큰 보내는 기능 삭제

--- src/widgets/join/model/usePostLogin.ts
+++ src/widgets/join/model/usePostLogin.ts
@@ -2,16 +2,12 @@ import { useMutation } from '@tanstack/react-query'
 import { setTokenFromCookie } from '@/shared'
 import { postLogin } from '../api/postLogin'
 import { UserInfoT } from '../api/user'
-import { sendTokenToRN } from '../utils/sendTokenToRN'
 
 function usePostLogin(userInfo: UserInfoT) {
   const { mutate } = useMutation({
     mutationKey: ['postLogin'],
     mutationFn: () => postLogin(userInfo),
-    onSuccess: res => {
-      sendTokenToRN(res.token)
-      setTokenFromCookie(res.token, 7)
-    },
+    onSuccess: res => setTokenFromCookie(res.token, 7),
   })
 
   return { mutate }
--- src/widgets/join/utils/sendTokenToRN.ts
+++
@@ -1,8 +0,0 @@
-import { sendRNFunction } from '@/shared'
-
-function sendTokenToRN(value: string) {
-  if (value.length === 0) sendRNFunction('getLoginToken', { result: 'fail' })
-  else sendRNFunction('getLoginToken', { result: 'success', token: value })
-}
-
-export { sendTokenToRN }
