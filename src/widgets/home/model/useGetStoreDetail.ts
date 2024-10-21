ae64daac458253dc3f6036c19d43cee11ad41977
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-10-22 01:38:22 +0900

    chore: fix typo

--- src/widgets/home/model/useGetStoreDetail.ts
+++ src/widgets/home/model/useGetStoreDetail.ts
@@ -4,12 +4,12 @@ import { storeNameAtom } from '@/shared/atoms/globalAtom'
 import { getStoreDetail } from '../api/getStoreDetail'
 import { noticeAtom } from '../atoms/homeAtoms'
 
-function useGetStoreDetail(stordId: string) {
+function useGetStoreDetail(storeId: string) {
   const [, setStoreName] = useAtom(storeNameAtom)
   const [, setNotice] = useAtom(noticeAtom)
   const { mutate } = useMutation({
     mutationKey: ['getStoreDetail'],
-    mutationFn: () => getStoreDetail(stordId),
+    mutationFn: () => getStoreDetail(storeId),
     onSuccess: res => {
       setStoreName(res.name)
       setNotice(res.internalNotice)
