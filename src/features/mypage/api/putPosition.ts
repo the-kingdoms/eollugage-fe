e23d75ce57ea12946b794890b48073a95dfa8644
Wonchang0314 <andrew0314@naver.com>
2024-10-22 00:48:37 +0900

    FIX: 사소한 에러 수정

--- src/features/mypage/api/putPosition.ts
+++ src/features/mypage/api/putPosition.ts
@@ -1,19 +1,10 @@
-import { axiosInstance, getTokenFromLocalStorage } from '@/shared'
+import { axiosInstance } from '@/shared'
 
 async function putPosition(storeId: string, memberId: string, position: string) {
-  const token = getTokenFromLocalStorage()
-  return axiosInstance.put(
-    `/v1/stores/${storeId}/relations/${memberId}`,
-    {
-      role: 'OWNER',
-      position: position,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    },
-  )
+  return axiosInstance.put(`/v1/stores/${storeId}/relations/${memberId}`, {
+    role: 'OWNER',
+    position: position,
+  })
 }
 
 export { putPosition }
