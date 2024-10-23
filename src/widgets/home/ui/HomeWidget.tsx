ceabe39b5d21cfe7d5fb4f554b1910df63f47846
PangKiYeon <pdrkeamy@gmail.com>
2024-10-23 16:49:05 +0900

    fix : homewidget atom set 기능 , prefetch 추가

--- src/widgets/home/ui/HomeWidget.tsx
+++ src/widgets/home/ui/HomeWidget.tsx
@@ -10,6 +10,8 @@ import { formatCurrentDate } from '@/features'
 import { useAtom } from 'jotai'
 import { isOwnerAtom } from '@/shared'
 import { useGetStoreInfo } from '@/entities'
+import { useEffect } from 'react'
+import { storeNameAtom } from '@/shared/atoms/globalAtom'
 import { useGetOrder } from '../model/useGetOrder'
 import { useGetTodayDuty } from '../model/useGetTodyDuty'
 import { noticeAtom } from '../atoms/homeAtoms'
@@ -20,19 +22,28 @@ interface HomeWidgetProps {
 
 export default function HomeWidget({ storeId }: HomeWidgetProps) {
   const [isOwner] = useAtom(isOwnerAtom)
-  const [notice] = useAtom(noticeAtom)
+  const [, setNotice] = useAtom(noticeAtom)
+  const [, setStoreName] = useAtom(storeNameAtom)
 
   const { data: orderList } = useGetOrder(storeId)
   const { data: workList } = useGetTodayDuty(storeId, formatCurrentDate())
-  const { data: storeInfo } = useGetStoreInfo(storeId)
+  const { data: storeInfo, prefetchStoreInfo } = useGetStoreInfo(storeId)
+
+  useEffect(() => {
+    prefetchStoreInfo()
+    if (storeInfo) {
+      setStoreName(storeInfo.name)
+      setNotice(storeInfo.internalNotice)
+    }
+  }, [storeInfo, prefetchStoreInfo])
 
   return (
     <>
-      <Header title={storeInfo?.name || ''} />
+      <Header title={storeInfo?.name || ''} storeCode={storeId.slice(0, 4)} />
       <div className="mt-[76px]">
-        {isOwner && <AddPhotoButton storeId={storeId} />}
+        {isOwner ? <AddPhotoButton storeId={storeId} /> : null}
         <FlexBox direction="col" className="gap-8 mx-4">
-          <HomeNotice storeId={storeId} notice={notice || ''} />
+          <HomeNotice storeId={storeId} notice={storeInfo?.internalNotice || ''} />
           <TodayWork workList={workList?.histories || []} />
           <OrderList storeId={storeId} orderList={orderList || []} />
         </FlexBox>
