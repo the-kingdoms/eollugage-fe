74a177c7d6fa1112a4ffb41e7adf2e8a3fb500f6
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-11-02 00:32:49 +0900

    fix: SSR app router query client로 변경

--- src/shared/ui/ProviderLayout.tsx
+++ src/shared/ui/ProviderLayout.tsx
@@ -1,13 +1,25 @@
 'use client'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider, isServer } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Provider } from 'jotai'
 import { DevTools } from 'jotai-devtools'
 import 'jotai-devtools/styles.css'
 
+function makeQueryClient() {
+  return new QueryClient({ defaultOptions: { queries: { staleTime: 60 * 1000 } } })
+}
+
+function getQueryClient() {
+  let browserQueryClient: QueryClient | undefined
+  if (isServer) return makeQueryClient()
+
+  if (!browserQueryClient) browserQueryClient = makeQueryClient()
+  return browserQueryClient
+}
+
 export default function ProviderLayout({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient()
+  const queryClient = getQueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>
