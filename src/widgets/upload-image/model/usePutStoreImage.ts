3aef94eddd953d3fe0e7585f91d4956189fbb1b5
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-11-07 04:09:56 +0900

    fix: 가게 이미지 업로드시 storeInfoAtom 업데이트

--- src/widgets/upload-image/model/usePutStoreImage.ts
+++ src/widgets/upload-image/model/usePutStoreImage.ts
@@ -1,11 +1,13 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { StoreInfoT, putStoreInfo } from '@/entities'
+import { StoreInfoT, putStoreInfo, storeInfoAtom } from '@/entities'
+import { useAtom } from 'jotai'
 
 function usePutStoreImage(
   imageName: string | undefined,
   storeInfo: StoreInfoT | undefined,
   storeId: string,
 ) {
+  const [, setStoreInfo] = useAtom(storeInfoAtom)
   const queryClient = useQueryClient()
   const newStoreInfo = storeInfo as StoreInfoT
   if (imageName && newStoreInfo) newStoreInfo.image = imageName
@@ -14,6 +16,7 @@ function usePutStoreImage(
     mutationKey: ['putStoreInfo', storeInfo],
     mutationFn: () => putStoreInfo(newStoreInfo, storeId),
     onSuccess: async () => {
+      setStoreInfo(newStoreInfo)
       await queryClient.refetchQueries({ queryKey: ['getStoreInfo', storeId] })
       await queryClient.refetchQueries({ queryKey: ['getPresignedURL', storeId] })
       await queryClient.refetchQueries({ queryKey: ['getStoreImage', storeId, imageName] })
