727d3c2fd9f08d8148c42c2bf136d2439f551436
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-11-08 00:15:53 +0900

    fix: 근무관리 탭 상단 글씨 가운데 정렬

--- src/widgets/work-management/ui/MemberSelector.tsx
+++ src/widgets/work-management/ui/MemberSelector.tsx
@@ -3,18 +3,19 @@ import { useMembers } from '@/entities'
 import MemberList from './MemberList'
 import { selectedMemberAtom } from '../atoms/workManagementAtoms'
 import formatPhoneNumber from '../utils/formatPhoneNumber'
+import { Flexbox } from '@/shared'
 
 export default function MemberSelector({ storeId }: { storeId: string }) {
   const { members } = useMembers(storeId)
   const selectedMemberID = useAtomValue(selectedMemberAtom)
   return (
-    <div className="h-[194px] pb-[16px] pt-[24px] bg-[#131313] text-white overflow-hidden">
-      <div className="flex space-x-[8px] mb-[16px] px-[16px] ">
+    <div className="h-[194px] pb-spacing-04 pt-spacing-06 bg-button-primary text-text-on-color overflow-hidden">
+      <Flexbox className="flex space-x-spacing-02 mb-spacing-04 px-spacing-04">
         <h2 className="body-03-bold-compact">근무자</h2>
-        <p className="body-01-medium-compact text-[#A8A8A8]">근무자를 선택해주세요</p>
-      </div>
+        <p className="body-01-medium-compact text-text-on-placeholder">근무자를 선택해주세요</p>
+      </Flexbox>
       <MemberList members={members} />
-      <p className="body-03-bold-compact mt-[12px] px-[16px]">
+      <p className="body-03-bold-compact mt-spacing-03 px-spacing-04">
         {selectedMemberID === null
           ? members && formatPhoneNumber(members[0].phoneNumber)
           : formatPhoneNumber(
