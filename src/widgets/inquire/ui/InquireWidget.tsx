a98aff86019d32f1ff82ca28b788ea8d3b2ab92e
KimYouGyoung <tptkds12@gmail.com>
2024-11-11 16:29:24 +0900

    FEAT: 카카오톡으로 문의하기 링크 연결

--- src/widgets/inquire/ui/InquireWidget.tsx
+++ src/widgets/inquire/ui/InquireWidget.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from 'next/navigation'
 import { TopBar, ButtonMobile, Icon } from '@eolluga/eolluga-ui'
+import Link from 'next/link'
 
 export default function InquireWidget({ storeId }: { storeId: string }) {
   const { push } = useRouter()
@@ -24,7 +25,9 @@ export default function InquireWidget({ storeId }: { storeId: string }) {
       </div>
 
       <footer className="w-full py-3 px-4 fixed bottom-4 flex flex-col gap-4">
-        <button
+        <Link
+          href="https://pf.kakao.com/_gxmxoIn/chat"
+          target="_blank"
           type="button"
           className="w-full h-[64px] py-spacing-05 px-spacing-07 gap-spacing-04 flex justify-center items-center bg-[#FEE500] text-text-on-color rounded-radius-04 shrink-0"
         >
@@ -32,7 +35,7 @@ export default function InquireWidget({ storeId }: { storeId: string }) {
             <Icon icon="kakaotalk_login" className="fill-text-on-color" />
           </span>
           <span className="text-black shrink-0">카카오톡으로 문의하기</span>
-        </button>
+        </Link>
         <ButtonMobile
           size="L"
           style="border"
