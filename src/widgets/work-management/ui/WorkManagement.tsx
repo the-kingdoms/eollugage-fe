7e09a1129c83a7a94b2b3a3dd1779d472f9a83de
KimYouGyoung <tptkds12@gmail.com>
2024-11-15 03:11:04 +0900

    FIX: 근무 관리 헤더(title) 부분이 근무자 리스트를 가리는 문제 수정 (모바일에서만 발생)

--- src/widgets/work-management/ui/WorkManagement.tsx
+++ src/widgets/work-management/ui/WorkManagement.tsx
@@ -7,7 +7,7 @@ export default async function WorkManagement({ storeId }: { storeId: string }) {
   if (userInfo === undefined) return null
 
   return userInfo.relationList.filter(relation => relation.storeId === storeId)[0].role ===
-    'Member' ? (
+    'OWNER' ? (
     <WorkManagementOwner storeId={storeId} />
   ) : (
     <WorkManagementMember storeId={storeId} userInfo={userInfo} />
