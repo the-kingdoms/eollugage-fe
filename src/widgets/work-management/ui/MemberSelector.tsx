0edf726bfe0f5c90168bebada1dbe7930520607f
KimYouGyoung <tptkds12@gmail.com>
2024-11-15 03:33:29 +0900

    FIX: 근무 관리 헤더 부분 가려지는 문제 재수정

--- src/widgets/work-management/ui/MemberSelector.tsx
+++ src/widgets/work-management/ui/MemberSelector.tsx
@@ -8,7 +8,7 @@ export default function MemberSelector({ storeId }: { storeId: string }) {
   const { members } = useMembers(storeId)
   const selectedMemberID = useAtomValue(selectedMemberAtom)
   return (
-    <div className="h-[194px] bg-[#131313] text-white overflow-hidden">
+    <div className=" bg-[#131313] text-white overflow-hidden pb-4">
       <div className="flex space-x-[8px] p-4 ">
         <h2 className="body-03-bold-compact">근무자</h2>
         <p className="body-01-medium-compact text-[#A8A8A8]">근무자를 선택해주세요</p>
