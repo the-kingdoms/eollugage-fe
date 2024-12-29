312ecc7c9370b604d97f39a0c6f3adf9a486d4b4
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-12-30 00:30:25 +0900

    fix: my api Response 변경에 따라 필요한 코드 원상복구

--- src/entities/user/types/user.ts
+++ src/entities/user/types/user.ts
@@ -5,6 +5,20 @@ export interface RelationT {
   position: string
   member: string | null
 }
+
+interface Store {
+  storeId: string
+  name: string
+  introduction: string
+  image: string
+  phone: string
+  address: string
+  openingHour: string
+  originalInfo: string
+  externalNotice: string
+  internalNotice: string
+}
+
 export interface UserInfo {
   id: string
   name: string
